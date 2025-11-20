818be4f7233dddefcdce9ddd94091a104398295f
sandeeptowtuwar <sandeephoney416@gmail.com>
2025-11-20 06:20:17 +0530

    Dev 2 added now code in app.java

--- app.java
+++ app.java
@@ -10,3 +10,21 @@ class Main {
     System.out.println(first + " + " + second + " = "  + sum);
   }
 }
+
+
+//New code started 
+
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
+
+
